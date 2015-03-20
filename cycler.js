54739c842d42a5e83c1c2ff2602f9fe7b804e31f
Jordan Curzon <curzonj@gmail.com>
2015-03-20 11:02:08 -0700

    add restart jitter to avoid platform overload

--- cycler.js
+++ cycler.js
@@ -14,6 +14,10 @@ if (process.env.HEROKU_ENDPOINT !== undefined) {
   });
 }
 
+function randomInt (low, high) {
+  return Math.floor(Math.random() * (high - low) + low);
+}
+
 function restart_hammers() {
   var app_name = process.env.RESTART_APP_NAME;
   var max_interval = process.env.MAX_LIFETIME ? parseInt(process.env.MAX_LIFETIME) : 1800000;
@@ -27,7 +31,7 @@ function restart_hammers() {
         var ddiff = Date.now() - new Date(dyno.updated_at).getTime();
         console.log(dyno.name, ddiff);
 
-        if(dyno.name.match(/hammer/) && ddiff > max_interval) { // 30min
+        if(dyno.name.match(/hammer/) && (ddiff - max_interval) > randomInt(1000, 60000)) { //add 1sec-1min of jitter
           console.log(dyno.name, 'should restart');
 
           client.app(app_name).dyno(dyno.id).restart();
